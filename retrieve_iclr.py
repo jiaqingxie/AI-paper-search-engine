e82c25b4c2310b1a604bd94610470a2bea58d169
JIAQING-XIE <s2001696@ed.ac.uk>
2022-01-04 07:37:19 +0000

    OK

---
+++ retrieve_iclr.py
@@ -0,0 +1,13 @@
+import time
+import urllib
+import random
+import os
+import requests
+import pandas as pd
+from selenium import webdriver
+from slugify import slugify
+from os import path
+from retrieve_titles import *
+
+cur_path = path.dirname(path.abspath(__file__))
+conference = "iclr"
